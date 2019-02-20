241ab4ec263df5136f0adf559a35369d06223cf0
ppriyadharsini <47592694+ppriyadharsini@users.noreply.github.com>
2019-02-20 11:59:43 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+void calculations(int a,int b,int c);
+int a,b,c;
+scanf("d\n",&a,&b);
+}
+ void calculations(int a,int b,int c)
+ {
+ int x,y,z;
+ x=a+b;
+ y=a-b;
+ z=a*b;
+ printf("%d\n%d\n%d\n",x,y,z);
+ }
+
